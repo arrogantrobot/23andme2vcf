3502d924e0efc67632dc8061342e68c6b6f56c5f
rlong <rob@buildtall.com>
2016-03-05 21:15:51 -0600

    functioning, no ouput to file yet

--- 23andme2vcf.py
+++ 23andme2vcf.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
 import sys
+import getopt
 import gzip
 
-reference_path = "23andme_v4_hg19_ref.txt.gz"
+from memoized import memoize
+
+RSID = 0
+CHROM = 1
+POS = 2
+CALLS = 3
+
+QUAL = "."
+FILTER = "."
+INFO = "."
+FORMAT = "GT"
 
 def usage(problem):
     print(problem)
@@ -11,12 +22,14 @@ def usage(problem):
     print("./23andme2vcf.py /path/to/unzipped/raw_data.txt /path/to/my/output.vcf\n")
     sys.exit()
 
-def get_reference():
+def get_reference(ref_ver):
+    reference_path = "23andme_v{0}_hg19_ref.txt.gz".format(ref_ver)
     try:
         with gzip.open(reference_path, 'rb') as f:
             ref_data = f.readlines()
     except IOError as e:
-        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        print("Error opening reference file")
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
         sys.exit()
     ref = {}
 
@@ -26,28 +39,80 @@ def get_reference():
 
     return ref
 
+def dump_missed_calls(missed_calls):
+    if len(missed_calls) > 10000:
+        print("There were more than 10,000 calls in the input data that could not be converted to VCF rows")
+        print("Try running 23andme2vcf with the v3 option:")
+        print("     ./23andme2vcf.py /path/to/input.txt /path/to/output.vcf --v3")
+        ""
+    try:
+        f = open("missed_calls.txt", 'w')
+    except IOError as e:
+        print("Error opening missed_calls.txt for output")
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+        sys.exit()
+    for call in missed_calls:
+        f.write(call)
+
 def get_input(path):
     try:
         f = open(path, 'r')
     except IOError as e:
-        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        print("Error opening raw data file")
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
         sys.exit()
     return f
 
-def convert_raw_data(input_path, output_path):
-    ref = get_reference()
+@memoize
+def get_alt_alleles(ref, calls):
+    allele_one = calls[0]
+    allele_two = calls[0] if len(calls)==1 else calls[1]
+    hom = allele_one == allele_two
+    if hom:
+        if ref.upper() == allele_one.upper():
+            return "0/0"
+        else :
+            return "1/1"
+    else:
+        if ref in calls:
+            return "0/1"
+        else:
+            return "1/2"
+    
+def convert_raw_data(input_path, output_path, ref_ver):
+    refs = get_reference(ref_ver)
     raw_calls = get_input(input_path)
-    for line in raw_calls.readlines():
-        print line.strip()
-    raw_calls.close()
+    vcf_rows = []
+    missed_calls = []
 
-    for line in ref.keys():
-        print(line, ref[line])
-    print(input_path, output_path)
-    return "nuts"
+    for line in raw_calls.readlines():
+        if "#" in line:
+            continue
+        (rsid, chrom, pos, calls) = line.strip().split()
+        ref = ""
+        if rsid in refs:
+            ref = refs[rsid]
+        else:
+            missed_calls.append(line.strip())
+            continue
+        alts = get_alt_alleles(ref,calls)
+        vcf_rows.append("\t".join([chrom, pos, rsid, ref, alts, QUAL, FILTER, INFO, FORMAT, "genotype"]))
+
+    raw_calls.close()
+    dump_missed_calls(missed_calls)
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "v")
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+        sys.exit(2)
+    ref_ver = "4"
+    for o, a in opts:
+        if o == "-v":
+            ref_ver = "3"
+    convert_raw_data(args[0], args[1], ref_ver)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        usage("wrong number of parameters")
-
-    convert_raw_data(sys.argv[1], sys.argv[2])
+    main()
---
+++ memoized.py
@@ -0,0 +1,13 @@
+import collections
+import functools
+
+class memoize(dict):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        return self[args]
+
+    def __missing__(self, key):
+        result = self[key] = self.func(*key)
+        return result
